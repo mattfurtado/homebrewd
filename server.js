aea9688eae7ba75d82d0a53281f4d6e73ce27aa0
Matt Furtado <mfurtado@hubspot.com>
2019-01-28 22:06:30 -0500

    Add endpoint to parse & return styleguide as JSON

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const fs = require('fs');
 const next = require('next');
+const xmlParser = require('xml2json');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -9,6 +11,13 @@ app
   .prepare()
   .then(() => {
     const server = express();
+
+    server.get('/api/styles', (req, res) => {
+      fs.readFile('./static/bjcp-styleguide.xml', (error, data) => {
+        if (error) throw error;
+        res.send(xmlParser.toJson(data));
+      });
+    });
 
     server.get('*', (req, res) => {
       return handle(req, res);
